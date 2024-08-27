71b4af865b782f2e9738651993c2120e329cbac7
JSenpa1 <41582843+JSenpa1@users.noreply.github.com>
2024-08-27 23:33:10 +0700

    real first commit

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,5 +1,6 @@
 import { Link, Head } from '@inertiajs/react';
-import { useGlitch } from 'react-powerglitch'
+import { useGlitch } from 'react-powerglitch';
+import { TypeAnimation } from 'react-type-animation';
 import '../../css/app.css';
 
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
@@ -14,8 +15,25 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
 
     return (
         <>
-            <div className='h-screen w-screen bg-black'>
-                <div className='flex h-screen justify-center items-center text-white text-[160px] font-extrabold'><span ref={glitch.ref}><span className='glow'>INFI</span><span className='text-orange-700 glow-orange'>N</span><span className='glow'>ITE</span></span></div>
+            <div className='w-screen h-screen bg-black flex flex-col justify-center items-center'>
+                <div className='text-white text-[160px] font-extrabold'><span ref={glitch.ref}><span className='glow'>INFI</span><span className='text-orange-700 glow-orange'>N</span><span className='glow'>ITE</span></span></div>
+                <TypeAnimation
+                    sequence={[
+                        'see you soon 24',
+                        3000, 
+                        'see you soon 23',
+                        3000,
+                        'see you soon 22',
+                        3000,
+                        'see you soon 21',
+                        3000
+                    ]}
+                    wrapper="span"
+                    speed={25}
+                    className='text-white glow'
+                    style={{ fontSize: '40px', display: 'inline-block' }}
+                    repeat={Infinity}
+                    />
             </div>
         </>
     );
