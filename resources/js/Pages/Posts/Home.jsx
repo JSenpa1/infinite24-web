22c3835510784ac3f29f6ed5cde93439657d364e
stevjoo <goodjestcom@gmail.com>
2024-09-16 15:26:13 +0700

    animasi

--- resources/js/Pages/Posts/Home.jsx
+++ resources/js/Pages/Posts/Home.jsx
@@ -1,3 +1,74 @@
+import { gsap } from "gsap/dist/gsap";
+
+gsap.from(".logoInfinite", {
+    duration: 2.5,
+    opacity: 0,
+    ease: "power2.out",
+});
+
+gsap.from(".globe", {
+    duration: 2.5,
+    opacity: 0,
+    ease: "power2.out",
+});
+
+gsap.from(".logo1", {
+    duration: 1.1,
+    opacity: 0,
+    delay: 0.3,
+    ease: "bounce.out",
+    y: -100,
+});
+
+gsap.from(".description", {
+    duration: 2.2,
+    opacity: 0,
+    ease: "elastic.out",
+    y: -100,
+});
+
+gsap.from(".photo1", {
+    duration: 2,
+    delay: 1,
+    ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })",
+    opacity: 0,
+});
+
+gsap.from(".tape1", {
+    duration: 1,
+    delay: 2,
+    ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })",
+    opacity: 0,
+});
+
+gsap.from(".photo2", {
+    duration: 2,
+    delay: 2.5,
+    ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })",
+    opacity: 0,
+});
+
+gsap.from(".tape2", {
+    duration: 1,
+    delay: 3.5,
+    ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })",
+    opacity: 0,
+});
+
+gsap.from(".photo3", {
+    duration: 2,
+    delay: 4,
+    ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })",
+    opacity: 0,
+});
+
+gsap.from(".tape3", {
+    duration: 1,
+    delay: 5,
+    ease: "rough({ strength: 1, points: 20, template: none.out, taper: none, randomize: true, clamp: false })",
+    opacity: 0,
+});
+
 function Home() {
     return (
         <div className="text-center bg-[url('/aboutus/background.png')]">
@@ -12,22 +83,22 @@ function Home() {
                 <img
                     src="/aboutus/infinite.svg"
                     alt="Infinite-banner"
-                    className="mt-24 ms-16 flex-none h-[108px] w-auto"
+                    className="logoInfinite mt-24 ms-16 flex-none h-[108px] w-auto"
                 />
                 <img
                     src="/aboutus/globe.svg"
                     alt="Globe"
-                    className="mt-5 flex-none h-[323.65px] w-auto"
+                    className="globe mt-5 flex-none h-[323.65px] w-auto"
                 />
             </div>
             <div className="absolute inset-72 flex justify-center z-20">
                 <img
                     src="/aboutus/title.svg"
                     alt="About Infinite"
-                    className="max-h-[66px] max-w-[668.53px]"
+                    className="logo1 max-h-[66px] max-w-[668.53px]"
                 />
             </div>
-            <div className="flex justify-center mt-9 font-[600] text-white text-2xl px-[229px]">
+            <div className="description flex justify-center mt-9 font-[600] text-white text-2xl px-[229px]">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 Vestibulum libero risus, posuere id porttitor et, pretium et
                 neque. Praesent ex nunc, tempus dignissim mauris id, vehicula
@@ -39,34 +110,34 @@ function Home() {
                 <div className="flex justify-center mt-6">
                     <img
                         src="/aboutus/tape1.svg"
-                        className="absolute me-24 mt-10 z-20 max-w-[46.39px]"
+                        className="tape1 absolute me-24 mt-10 z-20 max-w-[46.39px]"
                     />
                     <img
                         src="/aboutus/fotopleno1(2).JPG"
                         alt="Foto PPIF"
-                        className="max-w-[320px] max-h-[199px] border mt-16 border-8 border-white mx-12 rotate-[-13deg]"
+                        className="photo1 max-w-[320px] max-h-[199px] border mt-16 border-[12px] border-white mx-12 rotate-[-13deg]"
                     />
                 </div>
                 <div className="flex justify-center mt-0">
                     <img
                         src="/aboutus/tape2.svg"
-                        className="absolute ms-10 z-20 max-w-[116px] max-h-[116px]"
+                        className="tape2 absolute ms-10 z-20 max-w-[116px] max-h-[116px]"
                     />
                     <img
                         src="/aboutus/fotopleno1(1).JPG"
                         alt="Foto PPIF"
-                        className="max-w-[320px] max-h-[199px] mt-16 z-0 border border-8 border-white mx-12 rotate-[10deg]"
+                        className="photo2 max-w-[320px] max-h-[199px] mt-16 z-0 border border-[12px] border-white mx-12 rotate-[10deg]"
                     />
                 </div>
                 <div className="flex justify-center mt-10">
                     <img
                         src="/aboutus/tape3.svg"
-                        className="absolute me-24 ms-12 mt-0 z-20 max-w-[82px]"
+                        className="tape3 absolute me-24 ms-12 mt-0 z-20 max-w-[82px]"
                     />
                     <img
                         src="/aboutus/fotopleno1(3).JPG"
                         alt="Foto PPIF"
-                        className="max-w-[320px] mt-10 max-h-[199px] border border-8 border-white mx-12 rotate-[-17deg]"
+                        className="photo3 max-w-[320px] mt-10 max-h-[199px] border border-[12px] border-white mx-12 rotate-[-17deg]"
                     />
                 </div>
             </div>
