d9d5bc8e9f0f33b86917cb319c2f0f093c2b1049
Rilim06 <r.marvinlim@gmail.com>
2024-09-19 16:18:58 +0700

    Update api #2

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KodePosController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/auth', [KodePosController::class, 'getAuth']);
+
+Route::get('/checkKodePos', [KodePosController::class, 'getKodePos']);
