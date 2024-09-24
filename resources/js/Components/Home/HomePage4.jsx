b3c27a833adbd076d7c0066dcd8592ebe0676b67
Rafael54x <panglima.triton@gmail.com>
2024-09-24 19:25:50 +0700

    Update HomePage4.jsx

--- resources/js/Components/Home/HomePage4.jsx
+++ resources/js/Components/Home/HomePage4.jsx
@@ -12,46 +12,48 @@ const sponsor = getImageByName('sponsor');
 function HomePage4() {
   return (
     <div className="min-h-screen bg-grid-hero py-8 px-4">
-      <div className="flex flex-col justify-center items-center mt-32">
-      <img src={sponsor} className='absolute size-96' alt="" />
+      <div className="flex justify-center items-center mt-32">
+        <img src={sponsor} className="max-w-full h-auto" alt="Sponsor" />
       </div>
-      <div className="flex flex-col justify-center items-center mt-14">
-        <p className="text-dark-blue text-xl max-w-md font-bold mx-auto text-center my-auto">
+
+      <div className="flex justify-center items-center mt-14">
+        <p className="text-dark-blue text-xl max-w-md font-bold text-center">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero risus, 
           posuere id porttitor et, pretium et neque. Praesent ex nunc.
         </p>
       </div>
 
-        <div className="flex flex-wrap gap-6 max-w-6xl mx-auto justify-center items-center mt-20"> 
-        <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-3/4 h-96">
+      <div className="flex justify-center mt-20">
+        <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-full max-w-3xl h-40 sm:h-96">
         </div>
+      </div>
 
-        <div className="flex flex-wrap gap-6 max-w-6xl mx-auto justify-center items-center mt-20">
-        <div className="grid grid-cols-2 gap-6 flex-col justify-center items-center">
-        <div className="bg-cream border-4 mx-20 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-96 h-48">
+      <div className="flex justify-center mt-10 sm:mt-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-6xl">
+          <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 h-32 sm:h-48">
+          </div>
+          <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 h-32 sm:h-48">
+          </div>
         </div>
-        <div className="bg-cream border-4 mx-20 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-96 h-48">
-        </div>
-        </div>
+      </div>
 
-        <div className="flex flex-wrap gap-6 max-w-6xl mx-auto justify-center items-center mt-20">
-        <div className="grid grid-cols-3 gap-6">
-        <div className="bg-cream border-4 mx-5 my-5 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-80 h-40">
-        </div>
-        <div className="bg-cream border-4 mx-5 my-5 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-80 h-40">
-        </div>
-        <div className="bg-cream border-4 mx-5 my-5 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-80 h-40">
-        </div>
-        <div className="bg-cream border-4 mx-5 my-5 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-80 h-40">
-        </div>
-        <div className="bg-cream border-4 mx-5 my-5 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-80 h-40">
-        </div>
-        <div className="bg-cream border-4 mx-5 my-5 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-80 h-40">
-        </div>
-        </div>
-      </div>
-      </div>
-      </div>
+<div className="flex justify-center mt-10 sm:mt-20">
+  <div className="grid grid-cols-2 sm:grid-cols-3 gap-6 w-full max-w-6xl">
+    <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-full h-28 sm:h-40">
+    </div>
+    <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-full h-28 sm:h-40">
+    </div>
+    <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-full h-28 sm:h-40">
+    </div>
+    <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-full h-28 sm:h-40">
+    </div>
+    <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-full h-28 sm:h-40">
+    </div>
+    <div className="bg-cream border-4 border-dark rounded-3xl shadow-[12px_12px_0px_0px_rgba(0,0,0,1)] p-6 w-full h-28 sm:h-40">
+    </div>
+  </div>
+</div>
+
     </div>
   );
 }
