3d182e22f104b07af2d031c490641bb5debab3be
owin40 <godwinwoisiri40@gmail.com>
2024-09-26 23:57:39 +0700

    feat: paper footer, penyu, buku

--- resources/js/Components/Game/BoxToInputCode.jsx
+++ resources/js/Components/Game/BoxToInputCode.jsx
@@ -3,7 +3,7 @@ import { Image } from "@chakra-ui/react";
 export default function BoxToInputCode({ title, subtitle, action }) {
     return (
         //Parent dibikin w-screen
-        <div className="flex flex-col justify-center items-center w-3/4 lg:w-1/2 h-[400px] rounded-xl bg-white">
+        <div className="flex flex-col justify-center items-center w-3/4 lg:w-1/2 h-[400px] rounded-xl bg-white z-10">
             <form
                 action={action}
                 method="post"
