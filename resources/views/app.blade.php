efe5b8a6df039741141100a5b726f19d3eee23b1
JSenpa1 <41582843+JSenpa1@users.noreply.github.com>
2024-10-13 13:29:34 +0700

    update for seo

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <meta title="Infinite UMN">
+        <meta name="description" content="  Infinite adalah malam puncak yang wajib dihadiri oleh seluruh Mahasiswa Baru Informatika UMN angkatan 2024. Acara ini merupakan syarat kelulusan dari perkenalan prodi Informatika. Bergabunglah dalam momen bonding yang tak terlupakan dengan teman-teman seangkatan serta mahasiswa dari angkatan lain. Ini adalah kesempatan emas untuk memperluas jaringan dan berinteraksi dengan alumni yang telah sukses. Dihadiri dengan beragam permainan seru dan aktivitas menarik, Infinite akan memastikan bahwa kalian tetap excited dan terlibat sepanjang acara. Mari kita ciptakan kenangan indah dan hubungan yang kuat di malam yang penuh keceriaan ini!">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
