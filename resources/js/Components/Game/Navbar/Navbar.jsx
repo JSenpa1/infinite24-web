70f9cc3b82990ff3758e32636d1307e268fbbc89
JSenpa1 <41582843+JSenpa1@users.noreply.github.com>
2024-11-21 02:24:27 +0700

    Update Navbar.jsx

--- resources/js/Components/Game/Navbar/Navbar.jsx
+++ resources/js/Components/Game/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
+import whiteWord from '../../../../../public/Assets/logo_white_word.svg';
+
 export default function Navbar(){
     return(
         <div className="absolute top-0 w-screen h-fit">
             <div className="flex justify-center p-5 h-24">
                 <div className="flex justify-center items-center w-32">
-                    <img src="Assets/logo_white_word.svg" />
+                    <img src={whiteWord} />
                 </div>
             </div>
             <hr className="pb-1 border-t-2 border-white" />
